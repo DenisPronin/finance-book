285f5986bd3e51c1ca0a8c59f44871e71817473d
Denis <pronin.idenis@gmail.com>
2014-12-26 12:49:59 +0600

    refactor auth

---
+++ server/routes/auth.js
@@ -0,0 +1,37 @@
+var passport = require('passport');
+
+module.exports = function (app) {
+    app.get('/login', function (req, res) {
+        if(req.isAuthenticated()) {
+            res.redirect('/book');
+        }
+        else {
+            res.render('login', {
+                message: req.flash('loginMessage')
+            });
+        }
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/book', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    app.get('/sign_up', function (req, res) {
+        res.render('sign_up', {
+            message: req.flash('signupMessage')
+        });
+    });
+
+    app.post('/sign_up', passport.authenticate('local-signup', {
+        successRedirect : '/book', // redirect to the secure profile section
+        failureRedirect : '/sign_up', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
+};
