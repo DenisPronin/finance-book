9dcb13c57ba28db9a3abfe0158b0fb7a9c45a1a4
pronin <denis.pronin@sibers.com>
2015-01-26 16:15:07 +0600

    income date

--- public/js/app/book/income/incomeApi.js
+++ public/js/app/book/income/incomeApi.js
@@ -11,6 +11,10 @@
             var deferred = $q.defer();
 
             bookService.getTableData(mode).then(function(income) {
+                income.forEach(function(_income) {
+                    var date =  moment().set({'year': _income.year, 'month': _income.month_id, date: _income.day});
+                    _income.date = date.format('DD/MM/YYYY');
+                });
                 deferred.resolve(income);
             }, function() {
                 deferred.reject();
